96a4ef0bb42c852b6b25d9c13376e0c037cef929
SandaruwanB <sandarusbandara110@gmail.com>
2023-08-07 13:24:11 +0530

    DEV page navigations upcomming movies

--- admin/upcommingMovies.php
+++ admin/upcommingMovies.php
@@ -54,7 +54,7 @@
                                                 <td>'.$row['language'].'</td>
                                                 <td>
                                                     <a href="#" class="btn1"><i class="fa fa-eye"></i></a>
-                                                    <a href="/moviebooker/admin/customerEdit.php?uid='.$row['id'].'" class="btn1"><i class="fa fa-edit"></i></a>
+                                                    <a href="/moviebooker/admin/customerEdit.php?ufid='.$row['id'].'" class="btn1"><i class="fa fa-edit"></i></a>
                                                     <a href="/moviebooker/database/deluser.php?ufid='.$row['id'].'" class="btn1"  style="cursor : pointer;background : transparent; outline : none; border : none;"><i class="fa fa-trash-o"></i></a>
                                                 </td>
                                             </tr>';
